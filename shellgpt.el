fa9dfc6e7a7fa817cd033f8d85e26ede0ec0c6d7
bkc39 <pl.proofs@gmail.com>
2023-08-11 13:14:35 -0400

    remove macros?

--- shellgpt.el
+++ shellgpt.el
@@ -1,58 +1,43 @@
 (require 'comint)
 
 ;;;###autoload
-(defmacro defsgptcustom (id &rest stx)
-  "Calls `'defcustom`' on the identifier 'shellgpt:id' with the
-given rest syntax"
-  (let ((custom-id
-         (concat "shellgpt:" (symbol-name id))))
-    `(defcustom ,(intern custom-id) ,@stx)))
-
-;;;###autoload
-(defmacro defsgptfun (id &rest stx)
-  "Defines the function 'sgpt:id'"
-  (let ((custom-id
-         (concat "shellgpt:" (symbol-name id))))
-    `(defun ,(intern custom-id) ,@stx)))
-
-;;;###autoload
 (defgroup shellgpt nil
   "shellgpt package"
   :group 'ai)
 
 ;;;###autoload
-(defsgptcustom openai-config-file
+(defcustom shellgpt:openai-config-file
   "~/.openai"
   "File that stores the OPENAI_API_KEY"
   :type 'string
   :group 'shellgpt)
 
 ;;;###autoload
-(defsgptcustom executable-path
-  (or (executable-find "sgpt")
+(defcustom shellgpt:executable-path
+  (or (executable-find "")
       (progn
-        (warn "shellgpt executable not found in path (searched for 'sgpt')")
-        "sgpt"))
+        (warn "shellgpt executable not found in path (searched for '')")
+        ""))
   "Path of the shellgpt executable"
   :type 'list
   :group 'shellgpt)
 
 ;;;###autoload
-(defsgptcustom command-line-args
+(defcustom shellgpt:command-line-args
   (list "--repl" "emacs-repl")
-  "List of command line arguments passed to the sgpt process"
+  "List of command line arguments passed to the  process"
   :type 'list
   :group 'shellgpt)
 
 ;;;###autoload
-(defsgptcustom repl-buffer-name
+(defcustom shellgpt:repl-buffer-name
   "*shellgpt*"
   "Name of the buffer that contains the shellgpt process"
   :type 'string
   :group 'shellgpt)
 
 ;;;###autoload
-(defsgptfun fetch-api-key ()
+(defun shellgpt:fetch-api-key ()
   "Sets the OPENAI_API_KEY environment variable if not defined.
 
 Does this by reading the `'shellgpt:openai-config-file`' which
@@ -72,7 +57,7 @@ should be a single line containing the secret key.
       (message "openai config file does not exist. Exiting..."))))
 
 ;;;###autoload
-(defsgptfun start-repl ()
+(defun shellgpt:start-repl ()
   "Start the shellgpt REPL."
   (interactive)
   (let ((tmpbuf (get-buffer-create shellgpt:repl-buffer-name)))
@@ -89,7 +74,7 @@ should be a single line containing the secret key.
          (get-buffer-process tmpbuf)
          nil)))))
 
-(defsgptfun send-query-text (text &optional insert-newline)
+(defun shellgpt:send-query-text (text &optional insert-newline)
   "Send query text to a comint buffer.
 
 If insert-newline is non-nil a newline is appended to the text
@@ -105,7 +90,7 @@ which will submit the query to the REPL
              shellgpt:repl-buffer-name)))
 
 ;;;###autoload
-(defsgptfun quick-ask ()
+(defun shellgpt:quick-ask ()
   "Send a query string to the shellgpt repl.
 
 This will start the REPL if it has not been started already.
